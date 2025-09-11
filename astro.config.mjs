58bb05ae37654b19f9f2c6ca093a668ca7cc9c34
Botond Balla <ballabotond@ballabotond.com>
2025-09-11 08:34:16 +0200

    security updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ballabotond.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          hu: 'hu-HU',
+          it: 'it-IT',
+          zh: 'zh-CN',
+          ja: 'ja-JP',
+          egy: 'ar-EG'
+        }
+      }
+    })
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "hu", "it", "zh", "ja", "egy"],
